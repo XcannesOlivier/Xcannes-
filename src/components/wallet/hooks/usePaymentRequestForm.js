47cc03c3e9a15044ba60e2b5f385057810d7b0b5
xcannesOlivier <Desruelleolivier@gmail.com>
2026-01-26 16:08:10 +0000

    Update wallet components, payment request form, and locales; remove pay page

--- src/components/wallet/hooks/usePaymentRequestForm.js
+++ src/components/wallet/hooks/usePaymentRequestForm.js
@@ -5,14 +5,10 @@ import { useState } from "react";
 export function usePaymentRequestForm({
   defaultAmount = "",
   defaultCurrency = "XRP",
-  defaultMethod = "qr",
-  defaultToAddress = "",
   defaultMemo = "",
 } = {}) {
   const [requestAmount, setRequestAmount] = useState(defaultAmount);
   const [requestCurrency, setRequestCurrency] = useState(defaultCurrency);
-  const [requestMethod, setRequestMethod] = useState(defaultMethod); // 'qr' | 'link' | 'xrpl' | 'notification'
-  const [requestToAddress, setRequestToAddress] = useState(defaultToAddress);
   const [requestMemo, setRequestMemo] = useState(defaultMemo);
 
   return {
@@ -20,12 +16,7 @@ export function usePaymentRequestForm({
     setRequestAmount,
     requestCurrency,
     setRequestCurrency,
-    requestMethod,
-    setRequestMethod,
-    requestToAddress,
-    setRequestToAddress,
     requestMemo,
     setRequestMemo,
   };
 }
-
