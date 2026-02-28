11a5e13913b1e025d3a6f436bff0ceea3640d2bf
xcannesOlivier <Desruelleolivier@gmail.com>
2026-02-28 21:18:49 +0000

    nettoyage code mort frontend

--- src/components/wallet/hooks/useReceiveForm.js
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { useState } from "react";
-
-export function useReceiveForm({ defaultReceiveTab = "receive" } = {}) {
-  const [receiveTab, setReceiveTab] = useState(defaultReceiveTab); // 'receive' | 'request'
-
-  return { receiveTab, setReceiveTab };
-}
