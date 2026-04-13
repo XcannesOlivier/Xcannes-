74c18dd6500dda3553b8a5b4e0accb739327594f
xcannesOlivier <Desruelleolivier@gmail.com>
2026-04-13 10:05:01 +0000

    gros moove swap xrp

--- src/components/wallet/components/WalletDashboardFooter.jsx
+++ src/components/wallet/components/WalletDashboardFooter.jsx
@@ -6,7 +6,7 @@ const MOBILE_MODAL_BG = "#111516";
 const MOBILE_BASE_BG = "#090c0d";
 
 const mobileFooterStyle = {
-  paddingBottom: "14px",
+  paddingBottom: "0px",
 };
 
 const mobileOverlayFillStyle = {
