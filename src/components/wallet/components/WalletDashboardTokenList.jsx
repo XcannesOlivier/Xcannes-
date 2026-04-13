ce44a6255f94a44b2dabdbde15d22f9679d7550e
xcannesOlivier <Desruelleolivier@gmail.com>
2026-04-13 11:09:59 +0000

    UI WALLET

--- src/components/wallet/components/WalletDashboardTokenList.jsx
+++ src/components/wallet/components/WalletDashboardTokenList.jsx
@@ -47,10 +47,10 @@ export default function WalletDashboardTokenList({
         </div>
       )}
       <div className={listClassName}>
-        <div className="space-y-[4px] md:hidden">
+        <div className="space-y-[1px] md:hidden">
           {tokens.map(renderTokenRow)}
         </div>
-        <div className="hidden md:flex md:flex-col md:space-y-[4px]">
+        <div className="hidden md:flex md:flex-col md:space-y-[1px]">
           {tokens.map(renderTokenRow)}
         </div>
       </div>
