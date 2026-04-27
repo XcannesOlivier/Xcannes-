79c12fd770df027224f1dc45840cdc5c16f8620c
xcannesOlivier <Desruelleolivier@gmail.com>
2026-04-27 22:11:01 +0000

    UN PEU D'UI

--- src/components/wallet/modals/WalletDashboardReceiveModal.jsx
+++ src/components/wallet/modals/WalletDashboardReceiveModal.jsx
@@ -1386,7 +1386,7 @@ export default function WalletDashboardReceiveModal({
 	                                options={shareWalletOptions}
 	                                useNativeSelect={false}
 	                                portal
-	                                portalDesktopWidthOffset={1.5}
+	                                portalDesktopWidthOffset={2}
 	                                hideSelected
 	                                backdropClassName=""
 	                                iconClassName="inline-flex items-center justify-center leading-none"
@@ -1500,7 +1500,7 @@ export default function WalletDashboardReceiveModal({
 	                                options={shareWalletOptions}
 	                                useNativeSelect={false}
 	                                portal
-	                                portalDesktopWidthOffset={1.5}
+	                                portalDesktopWidthOffset={2}
 	                                hideSelected
 	                                backdropClassName=""
 	                                iconClassName="inline-flex items-center justify-center leading-none"
