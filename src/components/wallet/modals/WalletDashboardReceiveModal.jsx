b598c85427256755575d54906a5e109a7c46608b
xcannesOlivier <Desruelleolivier@gmail.com>
2026-05-01 18:59:10 +0000

    DROPDOWN PARAMETRES

--- src/components/wallet/modals/WalletDashboardReceiveModal.jsx
+++ src/components/wallet/modals/WalletDashboardReceiveModal.jsx
@@ -1507,7 +1507,7 @@ export default function WalletDashboardReceiveModal({
 
                           {shareWalletDropdownOpen && hasMultipleWallets ? (
                             <div
-                              className={`absolute left-0 right-0 top-full -mt-px ${accountDropdownMenuClassName}`}
+                              className={`wallet-receive-account-menu absolute left-0 right-0 top-full -mt-px ${accountDropdownMenuClassName}`}
                               onMouseDown={(e) => e.stopPropagation()}
                               onClick={(e) => e.stopPropagation()}
                               role="menu"
@@ -1817,7 +1817,7 @@ export default function WalletDashboardReceiveModal({
 
                             {requestWalletDropdownOpen && hasMultipleWallets ? (
                               <div
-                                className={`absolute left-0 right-0 top-full -mt-px ${accountDropdownMenuClassName}`}
+                                className={`wallet-receive-account-menu absolute left-0 right-0 top-full -mt-px ${accountDropdownMenuClassName}`}
                                 onMouseDown={(e) => e.stopPropagation()}
                                 onClick={(e) => e.stopPropagation()}
                                 role="menu"
