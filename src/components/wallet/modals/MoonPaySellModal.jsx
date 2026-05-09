15ce876afb5e9644724888d2459cb8877d8c01bb
xcannesOlivier <Desruelleolivier@gmail.com>
2026-05-09 19:39:29 +0000

    amelioration icone

--- src/components/wallet/modals/MoonPaySellModal.jsx
+++ src/components/wallet/modals/MoonPaySellModal.jsx
@@ -2157,7 +2157,7 @@ const MoonPaySellModal = ({
 
 			                <div className="flex h-10 items-center justify-center">
 			                  <span className={["inline-flex h-10 w-10 items-center justify-center rounded-2xl bg-transparent", sheetAccentFlowIconText].join(" ")}>
-			                      <ShieldCheckIcon className="h-[40px] w-[40px]" />
+			                      <ShieldCheckIcon className="h-[40px] w-[40px]" style={{ strokeWidth: 1 }} />
 			                    </span>
 			                </div>
 			                <div className="min-w-0 self-center">
