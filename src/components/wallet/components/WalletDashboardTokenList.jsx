5fd686fc4e74886a93ab5a17084c3f99c63c7f63
xcannesOlivier <Desruelleolivier@gmail.com>
2026-06-18 12:48:44 +0000

    un peu d'ui 17/06/26

--- src/components/wallet/components/WalletDashboardTokenList.jsx
+++ src/components/wallet/components/WalletDashboardTokenList.jsx
@@ -31,7 +31,7 @@ export default function WalletDashboardTokenList({
     >
       {(headerTitle || headerActionLabel) && (
         <div
-          className={`flex items-center gap-2 mb-0 md:mb-1 px-3 pt-0 bg-[linear-gradient(to_bottom,transparent_0%,#060809_calc(100%-8px),#060809_100%)] md:bg-[linear-gradient(to_bottom,transparent_0%,#0a0d0e_calc(100%-8px),#0a0d0e_100%)] ${
+          className={`flex items-center gap-2 mb-0 md:mb-1 px-3 pt-0 bg-transparent ${
             headerJustifyClass
           }`}
         >
